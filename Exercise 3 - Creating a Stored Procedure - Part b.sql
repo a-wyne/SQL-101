3cc62de43686e1f6c7dc4f15492f236691925fbb
a-wyne <89445278+a-wyne@users.noreply.github.com>
2021-12-19 16:27:47 +1100

    Add files via upload
    Final Project: Advanced SQL Techniques

---
+++ Exercise 3 - Creating a Stored Procedure - Part b.sql
@@ -0,0 +1,21 @@
+--#SET TERMINATOR @
+CREATE OR REPLACE PROCEDURE UPDATE_LEADERS_SCORE (
+IN in_School_ID INTEGER, 
+IN in_Leader_SCORE INTEGER)
+	
+LANGUAGE SQL
+MODIFIES SQL DATA
+	
+BEGIN
+	
+	UPDATE SCHOOLS
+	SET Leaders_Score = in_Leader_Score
+	WHERE School_ID = in_School_ID;
+	
+END
+
+@
+
+
+
+
---
+++ Exercise 4 - Using Transactions - part a.sql
@@ -0,0 +1,41 @@
+--#SET TERMINATOR @
+CREATE OR REPLACE PROCEDURE UPDATE_LEADERS_SCORE (
+IN in_School_ID INTEGER, 
+IN in_Leader_SCORE INTEGER)
+	
+LANGUAGE SQL
+MODIFIES SQL DATA
+	
+BEGIN
+	UPDATE SCHOOLS
+	SET Leaders_Score = in_Leader_Score
+	WHERE School_ID = in_School_ID;
+	
+	IF in_Leader_Score > 0 AND in_Leader_Score < 20 THEN
+	UPDATE SCHOOLS
+	SET Leaders_Icon = 'Very weak';
+	
+	ELSEIF in_Leader_Score < 40 THEN
+	UPDATE SCHOOLS
+	SET Leaders_Icon = 'Weak';
+		
+	ELSEIF in_Leader_Score < 60 THEN
+	UPDATE SCHOOLS
+	SET Leaders_Icon = 'Average';
+
+	ELSEIF in_Leader_Score < 80 THEN
+	UPDATE SCHOOLS
+	SET Leaders_Icon = 'Strong';
+
+	ELSEIF in_Leader_Score < 100 THEN
+	UPDATE SCHOOLS
+	SET Leaders_Icon = 'Very strong';
+
+	ELSE
+	ROLLBACK WORK;
+
+	END IF;
+	COMMIT WORK;
+
+END
+@
